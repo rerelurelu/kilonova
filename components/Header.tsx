c4f413e458ec81943593b21a416cde5410766a49
zoniha <awaawapurin@gmail.com>
2022-07-16 18:01:50 +0900

    ✨ feat: Add header

---
+++ components/Header.tsx
@@ -0,0 +1,57 @@
+import { Box, Flex, Link, ListItem, UnorderedList } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import NextLink from 'next/link';
+
+const entries = [
+  { href: '/', content: 'blog' },
+  { href: '/about', content: 'about' },
+] as const;
+
+const Header: NextPage = () => {
+  return (
+    <Box
+      as="header"
+      top="0"
+      left="0"
+      w="full"
+      zIndex={40}
+      bg="gray.900/70"
+      backdropBlur="8px"
+      boxShadow="md"
+      pos="fixed"
+      borderBottom={2}
+      borderBottomColor="cyan.200"
+    >
+      <Flex justify="space-between" alignItems="center" px={6} h={16} color="cyan.400">
+        <NextLink href="/">
+          <Link
+            fontWeight="bold"
+            fontSize="3xl"
+            _hover={{ textDecor: 'none' }}
+          >{`zoniha's blog`}</Link>
+        </NextLink>
+        <Box as="nav">
+          <UnorderedList display="flex" fontWeight="semibold" fontSize="lg" listStyleType="none">
+            {entries.map(({ href, content }) => {
+              return (
+                <ListItem key={href}>
+                  <NextLink href={href}>
+                    <Link
+                      textTransform="uppercase"
+                      px={3}
+                      _hover={{ textDecor: 'none', opacity: '0.7' }}
+                    >
+                      {content}
+                    </Link>
+                  </NextLink>
+                </ListItem>
+              );
+            })}
+          </UnorderedList>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Header;
