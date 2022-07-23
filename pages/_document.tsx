405e2a781772fc36fcc86b420dd6f2d184ca2cf4
zoniha <awaawapurin@gmail.com>
2022-07-23 14:25:28 +0900

    ☪️ refactor: Fix `meta`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,13 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+          <meta name="description" content="zoniha's blog" />
           <meta property="og:type" content="blog" />
           <meta property="og:title" content="zoniha's blog" />
-          <meta property="og:image" content="../public/og_image.png" />
+          <meta
+            property="og:image"
+            content="https://user-images.githubusercontent.com/43092452/180591686-565e96d1-4e0d-4650-96c5-9fd6cad6c517.png"
+          />
           <meta property="og:description" content="zonihaの個人サイトです。" />
           <meta property="og:site_name" content="zoniha's blog" />
         </Head>
