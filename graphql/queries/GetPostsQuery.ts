83769f554ce8db65decf35d28c345c1b8a4acf64
zoniha <awaawapurin@gmail.com>
2022-07-29 19:45:23 +0900

    ✨ feat: Display articles in descending order

--- graphql/queries/GetPostsQuery.ts
+++ graphql/queries/GetPostsQuery.ts
@@ -1,8 +1,8 @@
 import { DocumentNode, gql } from '@apollo/client';
 
 export const GET_POSTS_QUERY: DocumentNode = gql`
-  query Posts {
-    posts {
+  query GetPosts {
+    posts(orderBy: publishedAt_DESC) {
       createdAt
       title
       slug
