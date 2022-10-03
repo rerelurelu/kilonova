accc4a70d74c230e06f349bce511f87ddbb3c635
zoniha <awaawapurin@gmail.com>
2022-10-03 11:13:48 +0900

    feat: Use dummy data when developing

--- src/graphql/config/ApolloClientConfig.ts
+++ src/graphql/config/ApolloClientConfig.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const CONTENT_API = process.env.NEXT_PUBLIC_CONTENT_API;
+const isProduction = process.env.NODE_ENV === 'production';
+
+const CONTENT_API = isProduction
+  ? process.env.NEXT_PUBLIC_CONTENT_API
+  : process.env.NEXT_PUBLIC_CONTENT_DEV_API;
 
 const client = new ApolloClient({
   uri: CONTENT_API,
