0474142d8bb5194ad99b8b3ee27062fb1f8e8bce
zoniha <awaawapurin@gmail.com>
2022-10-04 11:43:21 +0900

    layout: Change background color

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -5,10 +5,16 @@ const theme = extendTheme({
     heading: `Overpass, "Noto Sans JP"`,
     body: `Overpass, "Noto Sans JP"`,
   },
+  colors: {
+    white: {
+      0: '#f4fbfecc',
+      50: '#f4fbfe',
+    },
+  },
   styles: {
     global: {
       body: {
-        backgroundColor: 'gray.900',
+        backgroundColor: 'white.50',
         color: 'cyan.200',
         letterSpacing: '0.0625rem',
       },
