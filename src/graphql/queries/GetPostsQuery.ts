322990d141b60ed2e32a9677898138d4fbc9da7c
zoniha <awaawapurin@gmail.com>
2022-10-04 17:52:16 +0900

    bug: Change the maximum number of articles fetched

--- src/graphql/queries/GetPostsQuery.ts
+++ src/graphql/queries/GetPostsQuery.ts
@@ -2,7 +2,7 @@ import { DocumentNode, gql } from '@apollo/client';
 
 export const GET_POSTS_QUERY: DocumentNode = gql`
   query GetPosts {
-    posts(orderBy: publishedAt_DESC) {
+    posts(orderBy: publishedAt_DESC, last: 30) {
       createdAt
       title
       slug
