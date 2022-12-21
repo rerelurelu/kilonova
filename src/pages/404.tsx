6077c23204a2bdda44c7bb64d3cae6ac3cd4f3c9
zoniha <awaawapurin@gmail.com>
2022-12-21 21:53:40 +0900

    feat: Add 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,13 @@ import { NextPage } from 'next';
 
 const Custom404: NextPage = () => {
   return (
-    <>
-      <h1>404</h1>
-    </>
+    <div className="grid place-items-center h-[70vh]">
+      <div className="flex items-center">
+        <p className="text-4xl text-white">404</p>
+        <div className="divider divider-horizontal h-12 before:bg-indigo-200 before:w-px after:bg-indigo-200 after:w-px"></div>
+        <p className="text-lg text-white">This page could not be found.</p>
+      </div>
+    </div>
   );
 };
 
