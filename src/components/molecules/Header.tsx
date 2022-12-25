ce8f3a424f3266d9ea4c6a6b296e1e1cdc322de4
zoniha <awaawapurin@gmail.com>
2022-12-25 23:50:25 +0900

    feat: Add Header

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -9,13 +9,13 @@ const entries = [
 
 const Header: NextPage = () => {
   return (
-    <div className="navbar font-semibold bg-base-100/30 backdrop-blur flex flex-row-reverse border-b border-indigo-500 fixed z-50">
+    <div className="navbar h-20 px-11 pt-4 font-semibold bg-base-100/30 backdrop-blur flex flex-row-reverse fixed z-50">
       <ul className="menu menu-horizontal p-0">
         {entries.map(({ href, content }) => {
           return (
             <li key={content}>
               <Link href={href} className="hover:bg-transparent">
-                <span className="text-lg uppercase antialiased hover:opacity-70">
+                <span className="text-lg capitalize antialiased tracking-widest hover:opacity-70">
                   {content}
                 </span>
               </Link>
