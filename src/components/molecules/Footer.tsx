13b9a7db2b8ab8c0ecae2a2abf0669c49ed0aae8
zoniha <awaawapurin@gmail.com>
2022-12-25 23:50:17 +0900

    feat: Add Footer

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 const Footer: NextPage = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center py-12 bg-base-100 text-base-content border-t border-indigo-900 tracking-widest">
       <div>
         <p>© 2022 zoniha</p>
       </div>
