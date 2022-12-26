3b2f33c904c96d6b86a2d8dc8097f2199b4c6ad0
zoniha <awaawapurin@gmail.com>
2022-12-27 00:45:42 +0900

    feat: Add PostCard component

--- src/components/molecules/PostCard.tsx
+++ src/components/molecules/PostCard.tsx
@@ -7,11 +7,16 @@ const PostCard: NextPage<PostCardProps> = ({ title, href, createdAt }) => {
   const dateDisplay = convertDateDisplay(createdAt.slice(0, 10));
 
   return (
-    <Link href={href}>
-      <div>
-        <p>Post Card</p>
+    <article className="card w-96 bg-blue-900 shadow-xl min-h-[12rem]">
+      <div className="card-body pr-16">
+        <header className="mb-auto">
+          <h2 className="card-title break-keep">
+            <Link href={href}>{title}</Link>
+          </h2>
+        </header>
+        <time dateTime={createdAt}>{dateDisplay}</time>
       </div>
-    </Link>
+    </article>
   );
 };
 
