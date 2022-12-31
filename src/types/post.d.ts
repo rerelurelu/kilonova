37d65d9e1776fce78ab4449e1db621dc2d4cce6e
zoniha <awaawapurin@gmail.com>
2022-12-31 20:34:11 +0900

    chore: Add types

---
+++ src/types/post.d.ts
@@ -0,0 +1,23 @@
+import { type } from 'os';
+
+export type Post = {
+  key: string;
+  link: string;
+  title: string;
+  createdAt: string;
+  tags: string[]
+};
+
+export type Posts = Post[];
+
+export type ZennPost = {
+  [key: string]: any;
+};
+
+export type BlogPost = {
+  __typename: string;
+  createdAt: string;
+  title: string;
+  slug: string;
+  id: string;
+};
