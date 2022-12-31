7f29fea362151038c7548d9646166a12683b89e8
zoniha <awaawapurin@gmail.com>
2023-01-01 01:16:37 +0900

    feat: Add post page

--- src/components/templates/MarkdownTemplate.tsx
+++ src/components/templates/MarkdownTemplate.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from 'next';
 import ReactMarkdown from 'react-markdown';
-// import style from '../../style/markdownTemplate.module.scss';
+import style from '../../styles/markdownTemplate.module.scss';
 
 export const MarkdownTemplate: NextPage<any> = ({ content }) => {
   return (
     <>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown className={style.markdown}>{content}</ReactMarkdown>
     </>
   );
 };
