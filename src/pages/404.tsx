0cad9d081c0c76c28c53d7645ae4e089fda8cf32
zoniha <awaawapurin@gmail.com>
2023-01-02 04:15:24 +0900

    style: Sort classNames

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,10 @@ import { NextPage } from 'next';
 
 const Custom404: NextPage = () => {
   return (
-    <div className="grid place-items-center h-[70vh]">
+    <div className="grid h-[70vh] place-items-center">
       <div className="flex items-center">
         <p className="text-4xl text-white">404</p>
-        <div className="divider divider-horizontal h-12 before:bg-indigo-200 before:w-px after:bg-indigo-200 after:w-px"></div>
+        <div className="divider divider-horizontal h-12 before:w-px before:bg-indigo-200 after:w-px after:bg-indigo-200"></div>
         <p className="text-lg text-white">This page could not be found.</p>
       </div>
     </div>
