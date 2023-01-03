1d5cb8f185e54d0b16c50966767065a41c356b8a
zoniha <awaawapurin@gmail.com>
2023-01-04 00:21:36 +0900

    layout: Update navbar design

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,21 +1,40 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useState } from 'react';
 
-const entries = [
+type Entries = 'blog' | 'about' | 'contact';
+
+const ENTRIES = [
   { href: '/', content: 'blog' },
   { href: '/about', content: 'about' },
   { href: '/contact', content: 'contact' },
 ] as const;
 
 const Header: NextPage = () => {
+  const [currentPage, setCurrentPage] = useState<Entries>('blog');
+
+  const handleClick = (e: any) => {
+    setCurrentPage(e.currentTarget.id);
+  };
   return (
     <div className="navbar fixed z-50 flex h-20 flex-row-reverse bg-base-100/30 px-11 pt-4 backdrop-blur">
       <ul className="menu menu-horizontal p-0">
-        {entries.map(({ href, content }) => {
+        {ENTRIES.map(({ href, content }) => {
           return (
             <li key={content}>
-              <Link href={href} className="hover:bg-transparent">
-                <span className="text-lg font-normal capitalize tracking-widest antialiased hover:opacity-70">
+              <Link
+                href={href}
+                className="hover:bg-transparent"
+                id={content}
+                onClick={handleClick}
+              >
+                <span
+                  className={`after:nav-underline relative text-lg font-semibold capitalize tracking-widest text-white antialiased ${
+                    currentPage === content
+                      ? 'after:w-full'
+                      : 'after:w-0 hover:after:w-full'
+                  }`}
+                >
                   {content}
                 </span>
               </Link>
